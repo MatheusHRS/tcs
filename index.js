0fea838f9855e8280421a82befa24ca8bb691e43
Matheus Henrique Ribeiro de Souza <matheus_1412@hotmail.com>
2019-03-24 22:23:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const fetch = require("node-fetch");
-
-fetch ("http://dontpad.com/testeTCSindustrial/matheus", {
-	method: "POST"
-})
+var request = require('request');
+request('http://dontpad.com/testeTCSindustrial/matheus', function (error, response, body, write) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode); 
+  console.log('body:', body);
+  console.log('write:', "Matheus"); 
+});
